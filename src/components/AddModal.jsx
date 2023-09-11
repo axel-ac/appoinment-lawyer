dafd9a7d225696dd968348abaacb1415cf193c22
axel-ac <abdullahceylan.axel@gmail.com>
2023-09-11 22:16:37 +0200

    addModal

--- src/components/AddModal.jsx
+++ src/components/AddModal.jsx
@@ -2,18 +2,10 @@ import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 
-function Example() {
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+function AddModal({show, handleClose}) {
 
   return (
     <>
-      <Button variant="primary" onClick={handleShow}>
-        Launch demo modal
-      </Button>
-
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>Modal heading</Modal.Title>
@@ -32,4 +24,4 @@ function Example() {
   );
 }
 
-export default Example;
+export default AddModal;
--- src/components/Lawyers.jsx
+++ src/components/Lawyers.jsx
@@ -1,8 +1,19 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import AddModal from "./AddModal";
+import { useState } from "react";
 
 const Lawyers = ({ lawyers }) => {
+      const [show, setShow] = useState(false);
+
+    //   const handleClose = () => setShow(false);
+    //   const handleShow = () => setShow(true);
+
+    const handleClick = () => {
+        // handleShow()
+        setShow(true)
+    }
   return (
     <Container className="p-2">
       <h3 className="display-6 mb-3" style={{ color: "rgb(50,10,200)" }}>
@@ -11,12 +22,15 @@ const Lawyers = ({ lawyers }) => {
       <Row className="justify-content-center">
         {lawyers.map((lw) => (
         <Col  key={lw.id} xs={6} sm={4} md={3} >
-            <img src={lw.img} alt={lw.img} className="img-thumbnail lawyer-img" />
+            <img src={lw.img} alt={lw.img} className="img-thumbnail lawyer-img"
+            onClick={handleClick} />
             <h5>{lw.name}</h5>
             <h6>{lw.dep}</h6>
         </Col>
         ))}
       </Row>
+      {/* <AddModal show={show} handleClose={handleClose} /> */}
+      <AddModal show={show} handleClose={() => setShow(false)} />
     </Container>
   );
 };
