518fc911d0ebc1fcd08e201db162ae1bda9647a9
axel-ac <abdullahceylan.axel@gmail.com>
2023-09-11 14:46:39 +0200

    start

---
+++ src/helper/data.jsx
@@ -0,0 +1,98 @@
+export const appointmentData = [
+  {
+    id: 1,
+    complainant: "Bankruptcy Lawyer",
+    day: new Date(),
+    consulted: true,
+    doctor: "Hazel Valery",
+  },
+  {
+    id: 2,
+    complainant: "Criminal Defense Lawyer",
+    day: new Date(),
+    consulted: false,
+    doctor: "Jessica White",
+  },
+  {
+    id: 3,
+    complainant: "Business Lawyer (Corporate Lawyer)",
+    day: new Date(),
+    consulted: true,
+    doctor: "Jasmine Carpanter",
+  },
+];
+
+export const doctorData = [
+  {
+    id: 1,
+    name: "Hazel Valery",
+    dep: "Bankruptcy Lawyer",
+    img: "https://cdn.pixabay.com/photo/2017/10/07/01/36/businesswoman-2825242_640.png",
+  },
+  {
+    id: 2,
+    name: "Jasmine Carpanter",
+    dep: "Business Lawyer (Corporate Lawyer)",
+    img: "https://cdn.pixabay.com/photo/2017/10/07/01/36/businesswoman-2825242_640.png",
+  },
+  {
+    id: 3,
+    name: "Arianna Mary",
+    dep: "Constitutional Lawyer",
+    img: "https://cdn.pixabay.com/photo/2017/10/07/01/36/businesswoman-2825242_640.png",
+  },
+  {
+    id: 4,
+    name: "Jessica White",
+    dep: "Criminal Defense Lawyer",
+    img: "https://cdn.pixabay.com/photo/2017/10/07/01/36/businesswoman-2825242_640.png",
+  },
+  {
+    id: 5,
+    name: "Harry Coming",
+    dep: "Employment and Labor Lawyer",
+    img: "https://cdn.pixabay.com/photo/2017/10/07/01/36/businesswoman-2825242_640.png",
+  },
+  {
+    id: 6,
+    name: "Kristensen Abraham",
+    dep: "Entertainment Lawyer",
+    img: "https://cdn.pixabay.com/photo/2017/10/07/01/36/businesswoman-2825242_640.png",
+  },
+  {
+    id: 7,
+    name: "Asley Graham",
+    dep: "Estate Planning Lawyer",
+    img: "https://cdn.pixabay.com/photo/2017/10/07/01/36/businesswoman-2825242_640.png",
+  },
+  {
+    id: 8,
+    name: "Mary Motley",
+    dep: "Family Lawyer",
+    img: "https://cdn.pixabay.com/photo/2016/07/17/21/44/the-figure-of-the-1524806_640.png",
+  },
+  {
+    id: 9,
+    name: "William Liam",
+    dep: "Immigration Lawyer",
+    img: "https://cdn.pixabay.com/photo/2016/07/17/21/44/the-figure-of-the-1524806_640.png",
+  },
+  {
+    id: 10,
+    name: "James Mateo",
+    dep: "Intellectual Property (IP) Lawyer",
+    img: "https://cdn.pixabay.com/photo/2016/07/17/21/44/the-figure-of-the-1524806_640.png",
+  },
+  {
+    id: 11,
+    name: "Henry Oliver",
+    dep: "Personal Injury Lawyer",
+    img: "https://cdn.pixabay.com/photo/2016/07/17/21/44/the-figure-of-the-1524806_640.png",
+  },
+  {
+    id: 12,
+    name: "Noah Motley",
+    dep: "Tax Lawyer",
+    img: "https://cdn.pixabay.com/photo/2016/07/17/21/44/the-figure-of-the-1524806_640.png",
+  },
+];
